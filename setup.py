5a0e2e64099805a59b130635f25e99b3c8bed39a
aman_mahajan <aman5@primenumbers.in>
2018-08-03 17:48:32 +0530

    README updated in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-long_description = read('README.rst')
+long_description = read('README.md')
 
 
 class PyTest(TestCommand):
